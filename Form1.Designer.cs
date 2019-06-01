3348bd8396d70cd47c8c97eefb87a08426dcf338
KonstantinMitish <mitikostya14@gmail.com>
2019-06-01 13:36:00 +0200

    Upload files

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace honeyNoSee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_play = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timerCreator = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_speed = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_play
+            // 
+            this.btn_play.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_play.Location = new System.Drawing.Point(12, 557);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(460, 193);
+            this.btn_play.TabIndex = 0;
+            this.btn_play.Text = "PLAY";
+            this.btn_play.UseVisualStyleBackColor = true;
+            this.btn_play.Click += new System.EventHandler(this.btn_play_Click);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // timerCreator
+            // 
+            this.timerCreator.Interval = 1000;
+            this.timerCreator.Tick += new System.EventHandler(this.timerCreator_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(460, 420);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Сбивай только смайлы Хаппы, Гнума - не трогай!\r\nСкорость:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // txt_speed
+            // 
+            this.txt_speed.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txt_speed.Location = new System.Drawing.Point(12, 453);
+            this.txt_speed.Name = "txt_speed";
+            this.txt_speed.Size = new System.Drawing.Size(460, 98);
+            this.txt_speed.TabIndex = 2;
+            this.txt_speed.Text = "100";
+            this.txt_speed.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(484, 762);
+            this.Controls.Add(this.txt_speed);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_play);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kappa Kappa Kappa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_play;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Timer timerCreator;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_speed;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace honeyNoSee
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public struct Peka
+        {
+            public string type;
+            public int number;
+            public float xPos;
+            public float yPos;
+            public float Angle;
+            public int Velosity;
+            public Peka(
+                string typeN,
+                    int numberN,
+                    float xPosN,
+                    float yPosN,
+                    float AngleN,
+                int Velo)
+            {
+                type = typeN;
+                number = numberN;
+                xPos = xPosN;
+                yPos = yPosN;
+                Angle = AngleN;
+                Velosity = Velo;
+            }
+        };
+      public List<Peka> pekas = new List<Peka>();
+        Graphics graph;
+        Color bg = Color.Yellow;
+        public bool ContainsPicture(int x1,int y1,Bitmap img)
+        {
+          //  this.BackColor = img.GetPixel(x1, y1);
+            return (img.GetPixel(x1,y1).A!=0);
+        }
+        Bitmap im;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            im = new Bitmap(500, 800);
+            graph = this.CreateGraphics();
+        }
+
+        private void RemoveInterface()
+        {
+            foreach(Control c in this.Controls)
+            {
+                c.Visible = false;
+            }
+        }
+
+        private void btn_play_Click(object sender, EventArgs e)
+        {
+            timer.Interval = 10000/ int.Parse(txt_speed.Text);
+            timerCreator.Interval =  timer.Interval*10;
+            RemoveInterface();
+            playing = true;
+         //   timer.Start();
+            timerCreator.Start();
+            //this.Invalidate();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+        }
+        void PekasClearExept(int e)
+        {
+            for (int i = 0; i < pekas.Count; i++)
+            {
+                if (i != e)
+                    pekas.RemoveAt(i);
+            }
+        }
+        bool playing = false;
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (playing)
+            {
+                for (int i = 0; i < pekas.Count; i++)
+                {
+                    pekas[i] = new Peka(pekas[i].type, pekas[i].number, pekas[i].xPos, pekas[i].yPos + pekas[i].Velosity, pekas[i].Angle,pekas[i].Velosity);
+                    if(pekas[i].yPos>=688)
+                    {
+                        if(pekas[i].type!="gnumme")
+                        {
+                            // timer.Stop();
+                            RectangleF rect = new RectangleF(pekas[i].xPos, pekas[i].yPos, 112.0f, 112.0f);
+                              graph.DrawRectangles(new Pen(Color.Red,10),new RectangleF[]{rect});
+                              PekasClearExept(i);
+                            timerCreator.Stop();
+                            playing = false;
+                            MessageBox.Show("САСАМБА");
+                            pekas.Clear();
+                            graph.Clear(Color.Yellow);
+                            label1.Visible = true;
+                            txt_speed.Visible = true;
+                            btn_play.Visible = true;
+                        }
+                        else if(pekas[i].yPos>=800)
+                        {
+                            pekas.RemoveAt(i);
+                        }
+                        if(pekas[i].yPos<=-120)
+                        {
+                            pekas.RemoveAt(i);
+                        }
+                    }
+                }
+                this.Invalidate();
+            }
+            else
+                graph.Clear(Color.Yellow);
+        }
+        Random Rnd = new Random();
+        private void timerCreator_Tick(object sender, EventArgs e)
+        {
+            switch(Rnd.Next(2))
+            { 
+                case 0:
+            pekas.Add(new Peka("gnumme",Rnd.Next(1,11),Rnd.Next(0,400),0,Rnd.Next(15),5));
+            break;
+                case 1:
+            pekas.Add(new Peka("happa",Rnd.Next(1,11),Rnd.Next(0,400),0,Rnd.Next(15),5));
+            break;
+        }}
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (playing)
+            {
+                //  graph.Clear(Color.Yellow);
+                foreach (Peka p in pekas)
+                {
+                    graph.DrawImage((Image)Properties.Resources.ResourceManager.GetObject(p.type + p.number.ToString()), new RectangleF(p.xPos, p.yPos, 112.0f, 112.0f));
+                }
+            }
+            else
+            {
+                graph.Clear(Color.Yellow);
+            }
+        }
+        string aliveType = "gnumme";
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+            int xx=e.X;
+            int yy = e.Y;
+            for (int i = 0; i < pekas.Count; i++)
+            {
+
+                RectangleF rect = new RectangleF(pekas[i].xPos, pekas[i].yPos, 112.0f, 112.0f);
+             //   graph.DrawRectangles(new Pen(Color.Red),new RectangleF[]{rect});
+             if(rect.IntersectsWith(new RectangleF(xx, yy, 1, 1)))
+             {
+                 if (ContainsPicture(xx - (int)(pekas[i].xPos), yy - (int)(pekas[i].yPos), (Bitmap)Properties.Resources.ResourceManager.GetObject(pekas[i].type + pekas[i].number.ToString())))
+                 {
+                     if (aliveType != pekas[i].type)
+                     {
+                         pekas[i] = new Peka(pekas[i].type, pekas[i].number, pekas[i].xPos, pekas[i].yPos, pekas[i].Angle, -20);
+                     }
+                     else
+                     {
+                         pekas.Clear();
+                        // timer.Stop();
+                         timerCreator.Stop();
+                         playing = false;
+                         graph.Clear(Color.Yellow);
+                         label1.Visible = true;
+                         btn_play.Visible = true;
+                         txt_speed.Visible = true;
+                         MessageBox.Show("САСАМБА");
+                     }
+                 }
+             }
+         }
+        }
+
+    }
+}
